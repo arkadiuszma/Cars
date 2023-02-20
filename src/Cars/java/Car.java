0c1a41908e76a2a1ef5d4083c88227074f1d5f18
arkadiuszma <amadejek@sii.pl>
2023-02-20 15:09:50 +0100

    add task 1.4

--- src/Cars/java/Car.java
+++ src/Cars/java/Car.java
@@ -4,7 +4,7 @@ public record Car(Manufacturer manufacturer, boolean isAutomaticGear, Market mar
                   List<Dimension> dimensions) {
 
     public static void getFilteredCars(List<Car> cars, String brandName, int minTrunkCapacity) {
-        cars.stream().filter(c -> c.isAutomaticGear&&
+        cars.stream().filter(c -> c.isAutomaticGear &&
                         c.manufacturer.brand().matches(brandName) &&
                         c.dimensions.stream().anyMatch(t -> t.trunkCapacity() > minTrunkCapacity))
                 .map(m -> m.market.countries().stream().map(country -> country.countryName() + "-" + country.countrySign()).toList()).forEach(System.out::println);
